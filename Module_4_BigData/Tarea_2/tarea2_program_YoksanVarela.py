763f81e2534d480b0f8bccd77841be233efb8441
Yoksan Varela Cambronero <yoksan.misc@gmail.com>
2024-06-24 12:34:03 -0600

    Adding second CSV

--- Module_4_BigData/Tarea_2/tarea2_program_YoksanVarela.py
+++ Module_4_BigData/Tarea_2/tarea2_program_YoksanVarela.py
@@ -1,9 +1,10 @@
 # General Imports
 import os
 import logging
+import shutil
 from library.yaml_parser import yaml_to_spark_df, yaml_file_parser
 from library.args_parser import yaml_files_loader
-from library.data_transformation import dataframe_union, product_count
+from library.data_transformation import dataframe_union, product_count, cashier_total_sell
 
 # Configuring logging
 logging.basicConfig(level=logging.INFO)
@@ -31,10 +32,10 @@ def elt_data():
             else:
                 df_all_cashiers = dataframe_union(df_all_cashiers,df_temporal_yaml)
                 if(df_all_cashiers == False):
-                    logger.info('Error during DF union, aborting')
+                    logger.error('Error during DF union, aborting')
                     return False
         else:
-            logger.info('Error during DF creation from YAML, aborting')
+            logger.error('Error during DF creation from YAML, aborting')
             return False
 
     # Showing Dataframe with all cashiers registered
@@ -43,11 +44,44 @@ def elt_data():
 
     return df_all_cashiers
 
+def create_csv(tmp_output_dir,file_name):
+    """ Function to change the CSV created by pySpark to a desire name
+
+    Args:
+        tmp_output_dir (directory): Temporal directory created to save CSV by pySpark
+        file_name (string): Desire name for CSV
+
+    Returns:
+        True/False: Boolean to indicate successful/error condition
+    """
+    for f in os.listdir(tmp_output_dir):
+        if('part-' in f):
+            try:
+                old_file_name = os.path.join(tmp_output_dir, f)
+                new_file_name = os.path.join(tmp_output_dir, file_name)
+                os.rename(old_file_name,new_file_name)
+                
+                # Copy file to Result folder
+                results_dir = './Results'
+                final_file_name = os.path.join(results_dir, file_name)
+                
+                # Copy the file
+                shutil.copy(new_file_name, final_file_name)
+            except Exception as ex:
+                print(f'Exception found: {ex}')
+                return False
+        else:
+            continue
+        
+    # Remove tmp directory
+    shutil.rmtree(tmp_output_dir)
+    return True
+
 def main():
     # Processing all YAML files
     df_all_sells = elt_data()
     if(df_all_sells == False):
-        logger.info('Error during ELT stage, aborting')
+        logger.error('Error during ELT stage, aborting')
         return False
 
     # Clean NaN and Nulls
@@ -57,26 +91,44 @@ def main():
     df_prod_total_amount = product_count(df_all_sells_cleanned)
 
     if(df_prod_total_amount != False):
-        df_prod_total_amount.show()
+        df_prod_total_amount.show(100)
         df_prod_total_amount.printSchema()
     else:
-        logger.info('Error during aggregation, aborting')
+        logger.error('Error during aggregation, aborting')
         return False
 
     # Creating one CSV file
-    output_dir = './Results'
-    df_prod_total_amount.coalesce(1).write.csv(output_dir, header=True)
+    output_dir_prod_total = './Results/tmp_prod_total'
+    df_prod_total_amount.coalesce(1).write.csv(output_dir_prod_total, header=True)
 
     # Renaming file to match desire name
     total_prod_filename = 'total_productos.csv'
-    for f in os.listdir(output_dir):
-        if('part-' in f):
-            old_file_name = os.path.join(output_dir, f)
-            new_file_name = os.path.join(output_dir, total_prod_filename)
-            os.rename(old_file_name,new_file_name)
-        elif('total_productos' in f):
-            continue
+    changing_name_status = create_csv(output_dir_prod_total, total_prod_filename)
+    if(changing_name_status == False):
+        logger.error('Error changing name from CSV file, aborting')
+        return False
 
+    # Second CSV: Total sold by cashier
+    df_cashier_total_amount = cashier_total_sell(df_all_sells_cleanned)
+
+    if(df_cashier_total_amount != False):
+        df_cashier_total_amount.show()
+        df_cashier_total_amount.printSchema()
+    else:
+        logger.error('Error during aggregation, aborting')
+        return False
+    
+    # Creating one CSV file
+    output_dir_cashier_total = './Results/tmp_cashier_total'
+    df_cashier_total_amount.coalesce(1).write.csv(output_dir_cashier_total, header=True)
+    
+    # Renaming file to match desire name
+    cashier_total_filename = 'total_cajas.csv'
+    changing_name_status = create_csv(output_dir_cashier_total, cashier_total_filename)
+    if(changing_name_status == False):
+        logger.error('Error changing name from CSV file, aborting')
+        return False
+    
     return True
 
 # Execute MAIN
--- Module_4_BigData/Tarea_2/unitary_tests/test_data_transformation.py
+++ Module_4_BigData/Tarea_2/unitary_tests/test_data_transformation.py
@@ -1,5 +1,5 @@
 # General Libraries
-from library.data_transformation import dataframe_union, product_count
+from library.data_transformation import dataframe_union, product_count, cashier_total_sell
 
 
 def test_concatenate_same_columns(spark_session):
@@ -72,7 +72,53 @@ def test_product_count_correct_sum(spark_session):
     assert df_aggregated.collect() == expected_ds.collect()
 
 def test_product_count_missing_column(spark_session):
-    """ Test the product count with a correct dataframe """
+    """ Test failure mode with a critical column is missing """
+    df_data = [(20, 1, 1, 'Aguacate', 550),
+               (20, 1, 2, 'Sandia', 1200),
+               (22, 1, 1, 'Jocote', 200),
+               (22, 2, 1, 'Sandia', 1200),
+               (24, 1, 1, 'Aguacate', 550)]
+    df_ds = spark_session.createDataFrame(df_data,['Numero_Caja', 
+                                                   'Numero_Compra', 
+                                                   'Numero_Producto', 
+                                                   'Nombre', 
+                                                   'Precio'])
+    
+    df_aggregated = product_count(df_ds)
+
+    assert df_aggregated == False
+    
+def test_cashier_total_sell_correct_sum(spark_session):
+    """ Test the correct sum per cashier """
+    df_data = [(20, 1, 1, 'Aguacate', 8, 550),
+               (20, 1, 2, 'Sandia', 1, 1200),
+               (22, 1, 1, 'Jocote', 20, 200),
+               (22, 2, 1, 'Sandia', 3, 1200),
+               (24, 1, 1, 'Aguacate', 2, 550)]
+    df_ds = spark_session.createDataFrame(df_data,['Numero_Caja', 
+                                                   'Numero_Compra', 
+                                                   'Numero_Producto', 
+                                                   'Nombre',
+                                                   'Cantidad',
+                                                   'Precio'])
+    
+    df_aggregated = cashier_total_sell(df_ds)
+
+    expected_ds = spark_session.createDataFrame(
+        [
+            (22, 7600),
+            (20, 5600),
+            (24, 1100),
+        ],
+        ['Numero_Caja', 'Total_Vendido'])
+    
+    expected_ds.show()
+    df_aggregated.show()
+
+    assert df_aggregated.collect() == expected_ds.collect()
+    
+def test_cashier_total_sell_missing_column(spark_session):
+    """ Test failure mode with a critical column is missing """
     df_data = [(20, 1, 1, 'Aguacate', 550),
                (20, 1, 2, 'Sandia', 1200),
                (22, 1, 1, 'Jocote', 200),
